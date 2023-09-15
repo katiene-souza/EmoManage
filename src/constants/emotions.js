978e5a3a370b9081bc35f713ce84ee9c1fc25333
Katiene Souza <katienestudy@gmail.com>
2023-09-15 12:03:01 -0300

    correção das constantes

--- src/constants/emotions.js
+++ src/constants/emotions.js
@@ -1,70 +1,70 @@
 export const emotions = {
 
-    ternura: "ternura é proximidade, afeto e compaixão, é um passo para o amor e pode ser despertada por pessoas, seres e objetos que não nos parecem ameaçadores.",
+    ternura: "É proximidade, afeto e compaixão, é um passo para o amor e pode ser despertada por pessoas, seres e objetos que não nos parecem ameaçadores.",
 
-    amor:"amor é o oposto de ódio e pode despertar um sorriso gigantesco ou lágrimas, existe dois tipos de amor: -AMOR ROMÂNTICO: quando pensamos muito em uma pessoa e sentimos nervorsismo e e alegria quando a vemos. -AMOR DILIGENTE: quando compartilhamos a alegria e a tristeza da pessoa que amamos e desejamos sempre o melhor para ela.",
+    amor: "É o oposto de ódio e pode despertar um sorriso gigantesco ou lágrimas, existe dois tipos de amor: • AMOR ROMÂNTICO: quando pensamos muito em uma pessoa e sentimos nervosismo e alegria quando a vemos.  • AMOR DILIGENTE: quando compartilhamos a alegria e a tristeza da pessoa que amamos e desejamos sempre o melhor para ela.",
 
     ódio: "ódio é uma antipatia, um sentimento de aversão por algo ou alguém, ficamos desejando que algo de ruim aconteça com o nosso objeto de ódio, pode durar muito ou pouco tempo. Quando o ódio motiva nossas ações, nos entregamos a raiva.",
 
-    raiva: "a raiva é passageira, nos domina por um tempo e depois vai embora, sentimos raiva quando achamos algo injusto ou uma ameaça ao nosso bem estar. A raiva nos avisa quando estamos diante de injustiça e agressões, mas ela bloqueia nossa capacidade de pensar com clareza, por isso é importante assumir o controle da raiva, mesmo quando sentimos irritação.",
+    raiva: "a raiva é passageira, nos domina por um tempo e depois vai embora, sentimos raiva quando achamos algo injusto ou uma ameaça ao nosso bem-estar.  A raiva nos avisa quando estamos diante de injustiça e agressões, mas ela bloqueia nossa capacidade de pensar com clareza,  sendo importante assumir seu controle, mesmo quando sentimos irritação.",
 
     irritação: "quando algo nos irrita, não conseguimos parar de prestar atenção nela, um incômodo passa a ser irritante quando achamos que não podemos mais suportá-la, irritação prolongada leva a tensão.",
 
     tensão: "a tensão também pode ser chamada de 'estresse' e acontece quando enfrentamos situações que podem ser ameaçadoras, ficamos nervosos, impacientes e perdemos a calma com facilidade, quando desabafamos com alguém que confiamos, sentimos alívio.",
 
-    alívio: "a sensasão de alívio é experimentada quando nos livramos de um peso, deixamos de nos sentir ameaçados ou quando pedimos desculpas, é deixar algo desagradável para trás. O alívio pode vir acompanhado de um relaxamento. Quando a tensão passa, voltamos a ficar tranquilos e em paz experimentando a serenidade.",
+    alívio: "a sensação de alívio é experimentada quando nos livramos de um peso, deixamos de nos sentir ameaçados ou quando pedimos desculpas, é deixar algo desagradável para trás. O alívio pode vir acompanhado de um relaxamento. Quando a tensão passa, voltamos a ficar tranquilos e em paz experimentando a serenidade.",
 
-    serenidade: "a serenidade é uma sensação de calma e harmonia. A pessoa que está serena é tranquila e delicada e facilita para que enxerguemos os acontecimentos com mais clareza. A serenidade pode ser treinada como um músculo e treiná-la ajuda você a despertar a felicidade.",
+    serenidade: "a serenidade é uma sensação de calma e harmonia. A pessoa que está serena é tranquila e delicada, temos facilidade para enxergar os acontecimentos com mais clareza. A serenidade pode ser treinada como um músculo e treiná-la ajuda você a despertar a felicidade.", 
 
     felicidade: "a felicidade é diferente para cada pessoa, ficamos felizes quando usamos nossas habilidades e talentos para fazer algo que somos bons ou que gostamos de fazer. A felicidade é uma sensação de satisfação com quem você é, não a confunda com alegria.",
 
-    alegria: "a alegria é despertada por alogo prazeroso e diferente da felicidade, ela dura pouco, porém podemos ter muitos momentos alegres durante o dia e ficamos com energia alta e somos mais positivos. O contrário da alegria é tristeza.",
+    alegria: "a alegria é despertada por algo prazeroso e diferente da felicidade, ela dura pouco, porém podemos ter muitos momentos alegres durante o dia, ficamos com energia alta e somos mais positivos. O contrário da alegria é tristeza.",
 
-    tristeza: "a tristeza é a diminuição geral do nosso ânimo, podemos perder o apetite, as forças, o desejo, motivação e a vontade de viver. As pessoas não ficam tristes pelos menos motivos, mas é comum sentirmos tristeza quando nos decepcionamos ou perdemos algo que era importante para nós. A tristeza é uma das faces da compaixação.",
+    tristeza: "a tristeza é a diminuição geral do nosso ânimo, podemos perder o apetite, as forças, o desejo, motivação e a vontade de viver. As pessoas não ficam tristes pelos menos motivos, mas é comum sentirmos tristeza quando nos decepcionamos ou perdemos algo que era importante para nós. A tristeza é uma das faces da compaixão.",
 
-    compaixão: "compaixação é o pesar/tristeza que sentimos pela infelicidade de alguém. Ela nos motiva a ajudar quem está sofrendo, seja um parente, desconhecido e até mesmo um personagem desconhecido. Quando não ajudamos alguém que está triste entramos no território do remorso.",
+    compaixão: "compaixão é o pesar/tristeza que sentimos pela infelicidade de alguém. Ela nos motiva a ajudar quem está sofrendo, seja um parente, desconhecido e até mesmo um personagem fictício. Quando não ajudamos alguém que está triste entramos no território do remorso.",
 
-    remorso: "algumas pessoas confudem arrependimento(mal-estar que sentimos depois de alguma ação, seja ela boa ou má) com remorso(surge quando fizemos algo que sabemos que é errado). O remorso não nos deixa pensar em nada além daquilo que fizemos de errado, e esse mal-estar que fica nos remoendo é a culpa.",
+    remorso: "algumas pessoas confundem arrependimento(mal-estar que sentimos depois de alguma ação, seja ela boa ou má) com remorso(surge quando fizemos algo que sabemos que é errado). O remorso não nos deixa pensar em nada além daquilo que fizemos de errado, e esse mal-estar que fica nos remoendo é a culpa.",
 
-    culpa: "sentimos culpa quando acreditamos que fizemos algo ruim. Ela nos ajuda a definir o que consideramos certo ou errado e nos permite avaliar nosso próprio comportamento e julgá-los, quando concluimos que fizemos algo errado, provavelmente sentiremos vergonha.",
+    culpa: "sentimos culpa quando acreditamos que fizemos algo ruim. Ela nos ajuda a definir o que consideramos certo ou errado e nos permite avaliar nosso próprio comportamento e julgá-los, quando concluímos que fizemos algo errado, provavelmente sentiremos vergonha.",
 
-    vergonha: "a sentimos quando sabemos que fizemos algo errado ou achamos que vão zombar da gente, podemos também sentir vergonha por algo que alguém fez. Ficamos vermelhos e o fato de que outras pessoas saibam que você está envergonhado pode provocar insegurança.",
+    vergonha: "a sentimos quando sabemos que fizemos algo errado ou achamos que vão zombar da gente, podemos também sentir vergonha por algo que alguém fez. Ficamos vermelhos e quando outras pessoas percebem que estamos envergonhados, a insegurança pode ser provocada.",
 
-    insegurança: "a insegurança é falta de confiaça em nós ou nos outros, quando nos sentimos inseguros, adotamos o mecanismo de defesa e às vezes por insegunça agimos com timidez.",
+    insegurança: "a insegurança é falta de confiança em nós ou nos outros, quando nos sentimos inseguros, adotamos o mecanismo de defesa e às vezes por insegurança agimos com timidez.",
 
-    timidez: "a timidez é um bloqueio que nos impede de agir com naturalidade, ela pode nos dominar diante de pessoas desconhecidas, poucos confiáveis ou ameaçadoras, ficamos desconfortáveis, desejaitados diante dessas situações e por medo de fazer algo errado, não falam nada, ficam quietos e tentam passar despercebido. Você pode ser tomado por uma sensação de confusão.",
+    timidez: "a timidez é um bloqueio que nos impede de agir com naturalidade, ela pode nos dominar diante de pessoas desconhecidas, pouco confiáveis ou ameaçadoras, ficamos desconfortáveis, desajeitados diante dessas situações e por medo de fazer algo errado, não falamos nada, ficamos quietos e tentamos passar despercebidos. E podemos ser tomados por uma sensação de confusão.",
 
-    confusão: "a confusão é uma mistura de sentimentos, você pode sentir raiva e ternura ao mesmo tempo. Experimentamos a confusão diante da desordem ou algo que não conseguimos entender completamente e é necessário que você tente enteder quais emoções estão presentes no momento. A confusão pode levar ao medo.",
+    confusão: "a confusão é uma mistura de sentimentos, você pode sentir raiva e ternura ao mesmo tempo. Experimentamos a confusão diante da desordem ou algo que não conseguimos entender completamente e é necessário que você tente entender quais emoções estão presentes no momento. A confusão pode levar ao medo.",
 
-    medo: "o medo surge quando você acha que vai sofrer algum dano. O medo pode se converter em pavor, o medo serve para nos deixar alerta diante de um perigo e o pavor nos paralisa e nos impede de pensar. Quando sentimos medo, nossos olhos ficam maiores para podermos ver melhor, o coração bombeia mais sangue para as pernas para podermos fugir. Normalmente o desconhecido nos causa medo, mas também pode causar perplexidade.",
+    medo: "o medo surge quando você acha que vai sofrer algum dano, ele pode se convertido em pavor, o medo serve para nos deixar alerta diante de um perigo e o pavor nos paralisa e nos impede de pensar. Quando sentimos medo, nossos olhos ficam maiores para podermos ver melhor, o coração bombeia mais sangue para as pernas para podermos fugir. Normalmente o desconhecido nos causa medo, mas também pode causar perplexidade.",
 
     perplexidade: "ficamos perplexos quando algo que considerávamos impossível se mostra verdadeiro, ela nos revela que estamos rodeados de pessoas e coisas fascinantes. A perplexidade vem da inocência e da curiosidade, mas quando vem acompanhada de repulsa pode se converter em aversão.",
 
-    aversão: "",
+    aversão: "É o desprazer que nos causa algo que consideramos repugnante. A aversão pode ser as mesmas para todas as pessoas ou não, porém ela nos mantém longe de substâncias que pode nos fazer mal. Manifestar sua aversão, pode despertar hostilidade em outras pessoas.",
 
-    hostilidade: "",
+    hostilidade: "Sentimos hostilidade quando alguém se opõe a nós ou a nossos desejos, tendemos a ser do contra e discordar de tudo o que a pessoa fala e passamos a rejeitá-la, porém, se acolhermos essa pessoa, desenvolvemos a aceitação.",
 
-    aceitação: "",
+    aceitação: "Nos sentimos aceitos quando as pessoas gostam de nós com tudo aquilo que somos e também quando somos reconhecidos e valorizados pelas nossas capacidades e ações. A falta de aceitação gera a incompreensão",
 
-    incompreensão: "",
+    incompreensão: "Nasce de um desajuste entre a forma que você enxerga o mundo e a maneira como as outras pessoas o veem. É uma mistura de frustração e desamparo.",
 
-    desamparo: "",
+    desamparo: "Surge quando nos sentimentos desprotegidos ou achamos que não podemos contar com o apoio de ninguém, sentimos pesar, tristeza e achamos que se pedirmos ajuda ninguém virá, podemos estar rodeados de pessoas e mesmo assim nos sentirmos desamparados, então surge a solidão",
 
-    solidão: "",
+    solidão: "A solidão é a ausência de companhia e pode ser útil em alguns momentos, podemos nos sentir sozinhos mesmo rodeado de pessoas que achamos que nos abandonou ou que não podemos contar, precisamos nos comunicar para vencê-la. Quando sentimos falta de momentos no qual não estávamos sozinhos, temos saudade.",
 
-    saudade: "",
+    saudade: "Quando parece que nos falta algo ou alguém, estamos falando da saudade, ela gera uma ausência que nos entristece. Se a saudade faz parte do seu dia a dia, você foi fisgado pela melancolia.",
 
-    melancolia: "",
+    melancolia: "Forma acentuada de saudade ou nostalgia. Quando estamos melancólicos parece que o mundo se torna um lugar distante e triste, ela nos domina em momentos de mudanças e podemos saboreá-la, mas caso esse prazer passar, surgirá a melancolia.",
 
-    tédio: "",
+    tédio: "Mistura de aborrecimento e cansaço que surge quando não estamos fazendo nada ou quando o que fazemos não nos satisfaz, o tempo parece demorar para passar e conseguimos burlá-la quando algo nos desperta expectativa.",
 
-    expectativa: "",
+    expectativa: "Esperança de que algo aconteça, podemos curtir as coisas com mais energia e ela desperta o entusiamo.",
 
-    entusiasmo: "",
+    entusiasmo: "Nos impulsiona a fazer algo com imenso prazer, ela nasce do seu interior e o impele a reagir e então chegamos à euforia.",
 
-    euforia: "",
+    euforia: "É uma energia positiva que nos dá força para enfrentar os momentos de adversidade ou festejar com um entusiamo maior que o normal. É uma sensação de bem-estar que nos deixa otimistas e é o oposto de desalento.",
 
-    desalento: "",
+    desalento: "Ela é sentida quando nos falta ânimo para seguir adiante, percebemos que estamos cansados e que não será tão fácil alcançar nossas metas, quando somos vencidos pelo desalento, abrimos caminho para a decepção.",
 
-    decepção: "",
+    decepção: "É o pesar que nos invade quando descobrimos que aquilo que acreditávamos não é verdade ou quando ficamos decepcionados com a esperança que havíamos depositado em algo ou alguém.",
 }
